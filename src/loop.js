86c414122c12c16f3b3daee64f37b324b86412b3
cyteon <129582290+Cyteon@users.noreply.github.com>
2024-10-24 21:00:40 +0200

    types column in monitors table and implement in loop.js

--- src/loop.js
+++ src/loop.js
@@ -122,58 +122,60 @@ export default async function start() {
 
         const start = Date.now();
 
-        await fetch(monitor.url)
-          .then(async (res) => {
-            if (okStatues.includes(res.status)) {
-              console.log(`Successfully pinged ${monitor.url}`);
-              const ping = Date.now() - start;
+        if (monitor.type == "web") {
+          await fetch(monitor.url)
+            .then(async (res) => {
+              if (okStatues.includes(res.status)) {
+                console.log(`Successfully pinged ${monitor.url}`);
+                const ping = Date.now() - start;
 
-              const avgPing = await db.get(
-                "SELECT AVG(ping) as avg FROM Pings WHERE id = ? AND (status = 'up' OR status = 'degraded')",
-                [monitor.id],
-              );
+                const avgPing = await db.get(
+                  "SELECT AVG(ping) as avg FROM Pings WHERE id = ? AND (status = 'up' OR status = 'degraded')",
+                  [monitor.id],
+                );
 
-              if (avgPing.avg != null) {
-                if (ping > avgPing.avg * data.degradedMultipiler) {
-                  await db.run(
-                    "INSERT INTO Pings (id, code, status, ping) VALUES (?, ?, ?, ?)",
-                    [monitor.id, res.status, "degraded", ping],
-                  );
+                if (avgPing.avg != null) {
+                  if (ping > avgPing.avg * data.degradedMultipiler) {
+                    await db.run(
+                      "INSERT INTO Pings (id, code, status, ping) VALUES (?, ?, ?, ?)",
+                      [monitor.id, res.status, "degraded", ping],
+                    );
+                  } else {
+                    await db.run(
+                      "INSERT INTO Pings (id, code, status, ping) VALUES (?, ?, ?, ?)",
+                      [monitor.id, res.status, "up", ping],
+                    );
+                  }
                 } else {
                   await db.run(
                     "INSERT INTO Pings (id, code, status, ping) VALUES (?, ?, ?, ?)",
                     [monitor.id, res.status, "up", ping],
                   );
                 }
-              } else {
-                await db.run(
-                  "INSERT INTO Pings (id, code, status, ping) VALUES (?, ?, ?, ?)",
-                  [monitor.id, res.status, "up", ping],
-                );
-              }
 
-              await sendEmbed(monitor, "up", ping);
-            } else {
-              console.error(`Failed to ping ${monitor.url}`);
+                await sendEmbed(monitor, "up", ping);
+              } else {
+                console.error(`Failed to ping ${monitor.url}`);
+
+                await db.run(
+                  "INSERT INTO Pings (id, code, status) VALUES (?, ?, ?)",
+                  [monitor.id, res.status, "down"],
+                );
+
+                await sendEmbed(monitor, "down");
+              }
+            })
+            .catch(async (err) => {
+              console.error(`Failed to ping ${monitor.url}: ${err}`);
 
               await db.run(
                 "INSERT INTO Pings (id, code, status) VALUES (?, ?, ?)",
-                [monitor.id, res.status, "down"],
+                [monitor.id, 0, "down"],
               );
 
               await sendEmbed(monitor, "down");
-            }
-          })
-          .catch(async (err) => {
-            console.error(`Failed to ping ${monitor.url}: ${err}`);
-
-            await db.run(
-              "INSERT INTO Pings (id, code, status) VALUES (?, ?, ?)",
-              [monitor.id, 0, "down"],
-            );
-
-            await sendEmbed(monitor, "down");
-          });
+            });
+        }
       }
     }
   }, 60000);
